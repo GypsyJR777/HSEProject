489040a837a91cc17ece20b02466535225ebef2e
Anthony <anthonybudin-18@yandex.ru>
2020-06-02 00:08:08 +0300

    chlen.def

---
+++ Scripts/main.py
@@ -0,0 +1,10 @@
+import chlen.def foo():
+    doc = "The  property."
+    def fget(self):
+        return self._
+    def fset(self, value):
+        self._ = value
+    def fdel(self):
+        del self._
+    return locals()
+ = property(**())
