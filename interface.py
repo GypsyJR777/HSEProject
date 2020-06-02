fe807b48b508691357ca370a84e8b0c51c9b5a89
Anthony <anthonybudin-18@yandex.ru>
2020-06-02 23:18:02 +0300

    window

--- interface.py
+++ interface.py
@@ -1,49 +1,89 @@
-from tkinter import * #
+# from tkinter import * #
+#
+# window = Tk()
+# window.title("База Данных")
+# window.geometry('900x540+300+200')
+# window.resizable(False, False)
+#
+# class Table(Frame):
+#     def __init__(self, parent=None, headings=tuple(), rows=tuple()):
+#         super().__init__(parent)
+#
+#         table = Treeview(self, show="headings", selectmode="browse")
+#         table["columns"]=headings
+#         table["displaycolumns"]=headings
+#
+#         for head in headings:
+#             table.heading(head, text=head, anchor=CENTER)
+#             table.column(head, anchor=CENTER)
+#
+#         for row in rows:
+#             table.insert('', END, values=tuple(row))
+#
+#         scrolltable = Scrollbar(self, command=table.yview)
+#         table.configure(yscrollcommand=scrolltable.set)
+#         scrolltable.pack(side=RIGHT, fill=Y)
+#         table.pack(expand=YES, fill=BOTH)
+#
+# class menu(object):
+#     """docstring for menu."""
+#
+#     def __init__(self, arg):
+#         super(menu, self).__init__()
+#         self.arg = arg
+#
+         # menu = Menu(window)  # меню (toolbar)
+         # new_item = Menu(menu, tearoff=0)
+         # new_item.add_command(label='Новый')  # новый элемент меню
+         # menu.add_cascade(label='Файл', menu=new_item)  # новый элемент внутри каскада меню
+         # window.config(menu=menu)
+#
+# frame_menu = Frame(window, width=900, height=100, bg='yellow')
+# frame_menu.grid(column=0,row=0)
+#
+# frame_bd = Frame(window, width=900, height=100, bg='blue')
+# frame_bd.grid(column=0,row=1)
+#
+# window.mainloop()
 
-window = Tk()
+
+
+
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class Table(tk.Frame):
+    def __init__(self, parent=None, headings=tuple(), rows=tuple()):
+        super().__init__(parent)
+
+        table = ttk.Treeview(self, show="headings", selectmode="browse")
+        table["columns"]=headings
+        table["displaycolumns"]=headings
+
+        for head in headings:
+            table.heading(head, text=head, anchor=tk.CENTER)
+            table.column(head, anchor=tk.CENTER)
+
+        for row in rows:
+            table.insert('', tk.END, values=tuple(row))
+
+        scrolltable = tk.Scrollbar(self, command=table.yview)
+        table.configure(yscrollcommand=scrolltable.set)
+        scrolltable.pack(side=tk.RIGHT, fill=tk.Y)
+        table.pack(expand=tk.YES, fill=tk.BOTH)
+
+
+window = tk.Tk()
 window.title("База Данных")
-window.geometry('900x540+300+200')
-
-menu = Menu(window)  # меню (toolbar)
-new_item = Menu(menu, tearoff=0)
+menu = tk.Menu()  # меню (toolbar)
+new_item = tk.Menu(menu, tearoff=0)
 new_item.add_command(label='Новый')  # новый элемент меню
 menu.add_cascade(label='Файл', menu=new_item)  # новый элемент внутри каскада меню
 window.config(menu=menu)
-
-# надо переписать табличку тут
-frame = Frame(window, relief=RAISED, borderwidth=1)
-frame.pack(fill=BOTH, expand=True)
-closeButton = Button(window, text="Close")
-closeButton.pack(side=RIGHT, padx=5, pady=5)
-okButton = Button(window, text="OK")
-okButton.pack(side=RIGHT)
-
-table_frame = Frame(window,borderwidth=1)
-table_frame.grid(column=0, row=0)
-
-table.tree = Treeview(self, columns = ('cod', 'proizv', 'strana', 'codtov', 'model', 'oc', 'vnutrpam', 'diagonal', 'proc', 'operpam', 'kolvo'),  height=40, show='headings')
-table.tree.column('cod', width=60, anchor=tk.CENTER)
-table.tree.column('proizv', width=60, anchor=tk.CENTER)
-table.tree.column('strana', width=60, anchor=tk.CENTER)
-table.tree.column('codtov', width=60, anchor=tk.CENTER)
-table.tree.column('model', width=60, anchor=tk.CENTER)
-table.tree.column('oc', width=60, anchor=tk.CENTER)
-table.tree.column('vnutrpam', width=60, anchor=tk.CENTER)
-table.tree.column('diagonal', width=60, anchor=tk.CENTER)
-table.tree.column('proc', width=60, anchor=tk.CENTER)
-table.tree.column('operpam', width=60, anchor=tk.CENTER)
-table.tree.column('kolvo', width=60, anchor=tk.CENTER)
-table.tree.heading('cod', text='код производителя')
-table.tree.heading('proizv', text='производитель')
-table.tree.heading('strana', text='страна')
-table.tree.heading('codtov', text='код товара')
-table.tree.heading('model', text='модель')
-table.tree.heading('oc', text='ОС')
-table.tree.heading('vnutrpam', text='внутренняя память')
-table.tree.heading('diagonal', text='диагональ экрана')
-table.tree.heading('proc', text='процессор')
-table.tree.heading('operpam', text='оперативаная память')
-table.tree.heading('kolvo', text='кол-во')
-table.tree.pack()
-
+table = Table(window, headings=('Код производителя', 'Производитель', 'Страна',
+'Код товара', 'Модель', 'Внутренняя память', 'Диагональ экрана', 'Процессор',
+'Оперативная память', 'Кол-во смартфонов'),
+rows=((123, 456, 789), ('abc', 'def', 'ghk')))
+table.pack(expand=tk.YES, fill=tk.BOTH)
 window.mainloop()
--- interface2.py
+++ interface2.py
@@ -1,13 +1,15 @@
-from tkinter import *
-from tkinter import Menu
-
+from tkinter import * #
 
 window = Tk()
 window.title("Добро пожаловать в приложение PythonRu")
 window.geometry('400x250')
+
+
 menu = Menu(window)
 new_item = Menu(menu, tearoff=0)
 new_item.add_command(label='Новый')
 menu.add_cascade(label='Файл', menu=new_item)
 window.config(menu=menu)
+
+
 window.mainloop()
