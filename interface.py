26e8691cd3ad108e525c3e3d1bc089b6a680bb30
Anthony <anthonybudin-18@yandex.ru>
2020-06-03 16:51:19 +0300

    frames

--- interface.py
+++ interface.py
@@ -1,54 +1,3 @@
-# from tkinter import * #
-#
-# window = Tk()
-# window.title("База Данных")
-# window.geometry('900x540+300+200')
-# window.resizable(False, False)
-#
-# class Table(Frame):
-#     def __init__(self, parent=None, headings=tuple(), rows=tuple()):
-#         super().__init__(parent)
-#
-#         table = Treeview(self, show="headings", selectmode="browse")
-#         table["columns"]=headings
-#         table["displaycolumns"]=headings
-#
-#         for head in headings:
-#             table.heading(head, text=head, anchor=CENTER)
-#             table.column(head, anchor=CENTER)
-#
-#         for row in rows:
-#             table.insert('', END, values=tuple(row))
-#
-#         scrolltable = Scrollbar(self, command=table.yview)
-#         table.configure(yscrollcommand=scrolltable.set)
-#         scrolltable.pack(side=RIGHT, fill=Y)
-#         table.pack(expand=YES, fill=BOTH)
-#
-# class menu(object):
-#     """docstring for menu."""
-#
-#     def __init__(self, arg):
-#         super(menu, self).__init__()
-#         self.arg = arg
-#
-         # menu = Menu(window)  # меню (toolbar)
-         # new_item = Menu(menu, tearoff=0)
-         # new_item.add_command(label='Новый')  # новый элемент меню
-         # menu.add_cascade(label='Файл', menu=new_item)  # новый элемент внутри каскада меню
-         # window.config(menu=menu)
-#
-# frame_menu = Frame(window, width=900, height=100, bg='yellow')
-# frame_menu.grid(column=0,row=0)
-#
-# frame_bd = Frame(window, width=900, height=100, bg='blue')
-# frame_bd.grid(column=0,row=1)
-#
-# window.mainloop()
-
-
-
-
 import tkinter as tk
 import tkinter.ttk as ttk
 
@@ -81,9 +30,47 @@ new_item = tk.Menu(menu, tearoff=0)
 new_item.add_command(label='Новый')  # новый элемент меню
 menu.add_cascade(label='Файл', menu=new_item)  # новый элемент внутри каскада меню
 window.config(menu=menu)
-table = Table(window, headings=('Код производителя', 'Производитель', 'Страна',
+
+
+# фрейм с кнопками управления
+frame_toolbar = tk.Frame(window, bg='red', bd=2)
+frame_toolbar.pack(side='top')
+
+# фрейм 1-ого элемента управления
+frame_box1 = tk.Frame(frame_toolbar, bg='purple', bd=2)
+frame_box1.pack()
+
+# фрейм 2-ого элемента управления
+frame_box2 = tk.Frame(frame_toolbar, bg='black', bd=2)
+frame_box2.pack()
+
+# фрейм 3-ого элемента управления
+frame_box3 = tk.Frame(frame_toolbar, bg='blue', bd=2)
+frame_box3.pack()
+
+# фрейм с табличкой
+frame_table = tk.Frame(window, bg='green', bd=2)
+frame_table.pack(side='bottom')
+
+# таблица
+table = Table(frame_table, headings=('Код производителя', 'Производитель', 'Страна',
 'Код товара', 'Модель', 'Внутренняя память', 'Диагональ экрана', 'Процессор',
 'Оперативная память', 'Кол-во смартфонов'),
-rows=((123, 456, 789), ('abc', 'def', 'ghk')))
+rows=((123, 456, 789, 7, 9, 10, 13), ('abc', 'def', 'ghk')))
 table.pack(expand=tk.YES, fill=tk.BOTH)
+
+
+
+button1=tk.Button(frame_box1, text=u'Первая кнопка')
+button2=tk.Button(frame_box2, text=u'Вторая кнопка')
+button3=tk.Button(frame_box3, text=u'Первая кнопка')
+#button4=tk.Button(frame_table, text=u'Вторая кнопка')
+button1.pack()
+button2.pack()
+button3.pack()
+#button4.pack()
+
+
+
+
 window.mainloop()
