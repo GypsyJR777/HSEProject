c368d2335f27973c3366557dfb6e805334987b2d
GypsyJR777 <ivan.demidov.01@list.ru>
2020-06-05 01:23:13 +0300

    main

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,74 +1,236 @@
-import sys
-sys.path.append('../Library/')
-
-
-
-import pickle
+import tkinter as tk
+import tkinter.ttk as ttk
 import pandas as pd
-from tkinter import Tk, Frame, Toplevel, Label, Entry, Button, Checkbutton, Scale, Canvas, Scrollbar, FLAT, GROOVE, N, S, E, W, IntVar, END, YES, BOTH, SOLID, LabelFrame
-from materials import road_to_data
-
-global base
-'''
-class Table(tk.Frame):
-    def __init__(self, parent=None, headings=tuple(), rows=tuple()):
-        super().__init__(parent)
-
-        table = ttk.Treeview(self, show="headings", selectmode="browse")
-        table["columns"]=headings
-        table["displaycolumns"]=headings
-
-        for head in headings:
-            table.heading(head, text=head, anchor=tk.CENTER)
-            table.column(head, anchor=tk.CENTER)
-
-        for row in rows:
-            table.insert('', tk.END, values=tuple(row))
-
-        scrolltable = tk.Scrollbar(self, command=table.yview)
-        table.configure(yscrollcommand=scrolltable.set)
-        scrolltable.pack(side=tk.RIGHT, fill=tk.Y)
-        table.pack(expand=tk.YES, fill=tk.BOTH)
-'''
-def import_lib(import_filename):
-    import pickle
-    from tkinter.messagebox import showerror
-    try:
-        libfile = open(import_filename, 'rb')
-    except:
-        showerror(title='Ошибка!', message="Файл не найден!")
-    else:
-        lib = pickle.load(libfile)
-        libfile.close()
-    return lib
-
-
-def show_base():
-    for widget in frame.winfo_children():
-        widget.destroy()
-    frame.grid_forget()
-    n = 0
-    Label(frame, text="Haзвание").grid(row=n, column=0, pady=5)
-    Label(frame, text="Автор").grid(row=n, column=1, pady=5)
-    Label(frame, text="Раздел").grid(row=n, column=2, pady=5)
-    Label(frame, text="Страницы").grid(row=n, column=3, pady=5)
-    Label(frame, text="Цена").grid(row=n, column=4, pady=5)
-    Label(frame, text="Год издания").grid(row=n, column=5, pady=5)
+from tkinter import messagebox as mb
 
 
 
+def Table(parent=None, xls=None):
+        global counter, tree, df
+        
+        df = pd.DataFrame(xls)
+        df_col = df.columns.values
+        
+        tree = ttk.Treeview(root)
+        tree["columns"]=(df_col)
+        counter = len(df)
+        index_col=False
+        #generating for loop to create columns and give heading to them through df_col var.
+        for x in range(10):
+            tree.column(x, width=50)
+            tree.heading(x, text=df_col[x])
+        #generating for loop to print values of dataframe in treeview column.
+        for i in range(counter):
+            tree.insert('', i, values=df.iloc[i,:].tolist())
+        tree.pack(expand=tk.YES, fill=tk.BOTH)
+
+
+def Table_add(firm, country, model, storage, diagonal, cpu, ram, amount, os):
+    global df, counter
+    
+    mdf.loc[counter] = [counter+1, firm, country, model, os, int(storage), diagonal, cpu, int(ram), int(amount)]
+    tree.destroy()
+    Table(root, mdf)
 
 
 
-window = tk.Tk()
-window.title("База Данных")
-menu = tk.Menu()  # меню (toolbar)
-new_item = tk.Menu(menu, tearoff=0)
-new_item.add_command(label='Новый')  # новый элемент меню
-menu.add_cascade(label='Файл', menu=new_item)  # новый элемент внутри каскада меню
-window.config(menu=menu)
-#table = Table(window, headings=('Код производителя', 'Производитель', 'Страна','Код товара', 'Модель', 'Внутренняя память', 'Диагональ экрана', 'Процессор','Оперативная память', 'Кол-во смартфонов'),rows=((123, 456, 789), ('abc', 'def', 'ghk')))
-table.pack(expand=tk.YES, fill=tk.BOTH)
-base = import_lib(road_to_data)
-show_base()
-window.mainloop()
+class Main(tk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+        self.init_main()
+
+    def init_main(self):
+        global mdf
+        w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+        root.geometry("%dx%d+0+0" % (w, h))
+        frame_toolbox = tk.Frame(root, bd=5)
+        frame_toolbox.pack(side='top', fill=tk.X)
+
+        # 1-st frame with controls
+        frame_box1 = tk.Frame(frame_toolbox, bd=5)
+        frame_box1.pack(side='left', fill=tk.Y, expand=1)
+
+        # 2-nd frame with controls
+        frame_box2 = tk.Frame(frame_toolbox, bd=5)
+        frame_box2.pack(side='left', fill=tk.Y, expand=1)
+
+        # 3-rd frame with controls
+        frame_box3 = tk.Frame(frame_toolbox, bd=5)
+        frame_box3.pack(side='left', fill=tk.Y, expand=1)
+
+        # frame for the Table
+        frame_table = tk.Frame(root, bd=2)
+        frame_table.pack(side='bottom')
+
+
+        # elemests of 1-st box
+        frame_box1_top = tk.Frame(frame_box1, bd=5)
+        frame_box1_bottom = tk.Frame(frame_box1, bd=5)
+        button1_box1=tk.Button(frame_box1_top, text=u'Добавить', command=self.open_dialog)
+        button2_box1=tk.Button(frame_box1_top, text=u'Правка')
+        button3_box1=tk.Button(frame_box1_bottom, text=u'Удалить')
+        button4_box1=tk.Button(frame_box1_bottom, text=u'Экспорт')
+
+
+        # pack elemests of 1-st box
+        frame_box1_top.pack(side='top')
+        frame_box1_bottom.pack(side='top')
+        button1_box1.pack(side='left')
+        button2_box1.pack(side='left')
+        button3_box1.pack(side='left')
+        button4_box1.pack(side='left')
+
+        # elemests of 2-nd box
+        button1_box2=tk.Button(frame_box2, text=u'Анализ')
+        button2_box2=tk.Button(frame_box2, text=u'Экспорт')
+
+        # pack elemests of 2-nd box
+        button1_box2.pack(side='left')
+        button2_box2.pack(side='left')
+
+        # elemests of 3-rd box
+        button1_box3=tk.Button(frame_box3, text=u'Первая кнопка', command=self.sorttest)
+        button2_box3=tk.Button(frame_box3, text=u'Вторая кнопка', command=self.sorttest2)
+
+        # pack elemests of 3-rd box
+        button1_box3.pack(side='left')
+        button2_box3.pack(side='left')
+
+        xls = pd.read_excel("../Data/Smartphones.xlsx")
+        mdf = pd.DataFrame(xls)
+        table = Table(root, mdf)
+
+
+        root.mainloop()
+
+
+    def open_dialog(self):
+        Child()
+
+    def sorttest(self):
+       # frame_table.delete
+        global df
+        ramsize = 8
+        df = mdf[mdf['RAM'] == ramsize]
+        tree.destroy()
+        table = Table(root, df)
+       
+        
+    def sorttest2(self):
+       # frame_table.delete
+        global df
+        tree.destroy()
+        stor = 256
+        df = mdf[mdf['Storage'] == stor]
+        table = Table(root, df)
+
+
+
+class Child(tk.Toplevel):   
+    
+    def __init__(self):
+        super().__init__(root)
+        self.init_child()
+
+
+    def init_child(self):
+        self.title('Добавление')
+        self.geometry('260x260+400+300')
+        self.resizable(False, False)
+
+#self.entry_firm, self.entry_country, self.entry_model, self.entry_storage, self.entry_diagonal, self.entry_cpu, self.entry_ram, self.entry_amount, self.combobox
+        def add():
+            global counter, df
+            if(entry_firm.get()!='' and entry_country.get()!='' and 
+               entry_model.get() !='' and entry_storage.get() !='' and 
+               entry_diagonal.get() !='' and entry_cpu.get()!='' and 
+               entry_ram.get() !='' and entry_amount.get() !='' and 
+               combobox.get()!=''):
+                print(entry_firm)
+                if entry_ram.get().isdigit() == False:
+                    mb.showerror("Ошибка", "Должны быть введены числа в полях 'Память', 'Оперативная память' и 'Количество'")
+                else:
+                    Table_add(entry_firm.get(), entry_country.get(), entry_model.get(), entry_storage.get(), entry_diagonal.get(), entry_cpu.get(), entry_ram.get(), entry_amount.get(), combobox.get())
+                
+
+
+        label_description = ttk.Label(self, text='Операционная система')
+        label_description.grid(row=10, column = 0)
+
+#        label_description = ttk.Label(self, text='Код товара')
+#        label_description.grid(row=1, column =0)
+
+        label_description = ttk.Label(self, text='Производитель')
+        label_description.grid(row=2, column =0)
+
+        label_description = ttk.Label(self, text='Страна')
+        label_description.grid(row=3, column =0)
+
+        label_description = ttk.Label(self, text='Модель')
+        label_description.grid(row=4, column =0)
+
+        label_description = ttk.Label(self, text='Память')
+        label_description.grid(row=5, column =0)
+
+        label_description = ttk.Label(self, text='Диагональ')
+        label_description.grid(row=6, column =0)
+
+        label_description = ttk.Label(self, text='Процессор')
+        label_description.grid(row=7, column =0)
+
+        label_description = ttk.Label(self, text='Оперативная память')
+        label_description.grid(row=8, column =0)
+
+        label_description = ttk.Label(self, text='Количество')
+        label_description.grid(row=9, column =0)
+
+#        self.entry_cod = ttk.Entry(self)
+#        self.entry_cod.grid(row=1, column=1)
+
+        entry_firm = ttk.Entry(self)
+        entry_firm.grid(row=2, column=1)
+
+        entry_country = ttk.Entry(self)
+        entry_country.grid(row=3, column=1)
+
+        entry_model = ttk.Entry(self)
+        entry_model.grid(row=4, column=1)
+
+        entry_storage = ttk.Entry(self)
+        entry_storage.grid(row=5, column=1)
+
+        entry_diagonal = ttk.Entry(self)
+        entry_diagonal.grid(row=6, column=1)
+
+        entry_cpu = ttk.Entry(self)
+        entry_cpu.grid(row=7, column=1)
+
+        entry_ram = ttk.Entry(self)
+        entry_ram.grid(row=8, column=1)
+
+        entry_amount = ttk.Entry(self)
+        entry_amount.grid(row=9, column=1)
+
+        combobox = ttk.Combobox(self, values=[u'Android',u'IOS', u'BlackBerry'], width=17)
+        combobox.grid(row=10, column=1)
+
+        btn_cancel = ttk.Button(self, text='Отмена', command=self.destroy)
+        btn_cancel.grid(row=13, column=0, columnspan=2)
+        
+        btn_add = ttk.Button(self, text='Добавить', command=add)
+        btn_add.grid(row=12, column=0, columnspan=2)
+        btn_add.bind('<Button-1>')
+
+        self.grab_set()
+        self.focus_set()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Main(root)
+    app.pack()
+    root.title("Программа")
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.geometry("%dx%d+0+0" % (w, h))
+    root.resizable(False, False)
+
