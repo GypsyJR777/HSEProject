197ee72040fea09f4a87c03d7f79bc0c4061f752
Anthony <anthonybudin-18@yandex.ru>
2020-06-14 16:56:27 +0300

    bd

--- Scripts/bd.py
+++ Scripts/bd.py
@@ -10,6 +10,17 @@ import tkinter.ttk as ttk
 import pandas as pd
 import app as m
 
+def treeview_sort_column(tv, col, reverse):
+    l = [(tv.set(k, col), k) for k in tv.get_children()]
+    l.sort(reverse=reverse)
+
+    # rearrange items in sorted positions
+    for index, (val, k) in enumerate(l):
+        tv.move(k, '', index)
+
+    # reverse sort next time
+    tv.heading(col, command=lambda: treeview_sort_column(tv, col, not reverse))
+
 
 def Table(parent=None, xls=None):
     '''
@@ -28,8 +39,12 @@ def Table(parent=None, xls=None):
     for head in headings:
         tree.heading(head, text=head, anchor=tk.CENTER)
         tree.column(head, anchor=tk.CENTER, width=50)
+        tree.heading(head,text=head.capitalize(),command=lambda head_=head: treeview_sort_column(tree, head_, False))
     for i in range(count):
         tree.insert('', i, values=df.iloc[i, :].tolist())
+
+    # lststolb = [int(item) for item in list(m.mdf[first_stolb.get()])]
+
     scrollbar = tk.Scrollbar(tree, orient="vertical", command=tree.yview)
     tree.configure(yscrollcommand=scrollbar.set)
     scrollbar.pack(side="right", fill="y")
