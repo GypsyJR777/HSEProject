956539174545f7f79bb03aad58d9c7c1c137231a
Anthony <anthonybudin-18@yandex.ru>
2020-06-23 21:14:16 +0300

    tooltip

--- Scripts/graphs.py
+++ Scripts/graphs.py
@@ -14,6 +14,60 @@ import app as m
 import numpy as np
 from tkinter import messagebox as mb
 
+class CreateToolTip(object):
+    """
+    create a tooltip for a given widget
+    """
+    def __init__(self, widget, text='widget info'):
+        self.waittime = 500     #miliseconds
+        self.wraplength = 300   #pixels
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+        self.widget.bind("<ButtonPress>", self.leave)
+        self.id = None
+        self.tw = None
+
+    def enter(self, event=None):
+        self.schedule()
+
+    def leave(self, event=None):
+        self.unschedule()
+        self.hidetip()
+
+    def schedule(self):
+        self.unschedule()
+        self.id = self.widget.after(self.waittime, self.showtip)
+
+    def unschedule(self):
+        id = self.id
+        self.id = None
+        if id:
+            self.widget.after_cancel(id)
+
+    def showtip(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + 20
+        # creates a toplevel window
+        self.tw = tk.Toplevel(self.widget)
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(self.tw, text=self.text, justify='left',
+                       background="#ffffff", relief='solid', borderwidth=1,
+                       wraplength = self.wraplength)
+        label.grid(column=2,row=1)
+
+    def hidetip(self):
+        tw = self.tw
+        self.tw= None
+        if tw:
+            tw.destroy()
+
+
 class Kowalski_analis(tk.Toplevel):
     def __init__(self, parent_):
         '''
@@ -47,8 +101,8 @@ class Kowalski_analis(tk.Toplevel):
             plt.show()
 
 
-        def stolb_info(event):
-            mb.showinfo("Описание","Показывает график, представленный прямоугольными зонами, высоты которых пропорциональны величинам, которые они отображают.")
+        # def stolb_info(event):
+        #     mb.showinfo("Описание","Показывает график, представленный прямоугольными зонами, высоты которых пропорциональны величинам, которые они отображают.")
 
 
 
@@ -67,8 +121,8 @@ class Kowalski_analis(tk.Toplevel):
             data_pt.to_excel(export_file)
 
 
-        def svod_info(event):
-            mb.showinfo("Описание","Резюмирует данные, представленные в исходной таблице, показывает среднее значение по конечному столбцу.")
+        # def svod_info(event):
+        #     mb.showinfo("Описание","Резюмирует данные, представленные в исходной таблице, показывает среднее значение по конечному столбцу.")
 
 
 
@@ -94,8 +148,8 @@ class Kowalski_analis(tk.Toplevel):
             plt.show()
 
 
-        def rasseivanie_info(event):
-            mb.showinfo("Описание","Показывает взаимосвязь между двумя или тремя переменными, цвет и размер точек отображают значение третьей переменную. Если введены 2 параметра, вместо третьего используется количество повторений.")
+        # def rasseivanie_info(event):
+        #     mb.showinfo("Описание","Показывает взаимосвязь между двумя или тремя переменными, цвет и размер точек отображают значение третьей переменную. Если введены 2 параметра, вместо третьего используется количество повторений.")
 
 
         def analis_baz(event):
@@ -110,8 +164,8 @@ class Kowalski_analis(tk.Toplevel):
             bazstat.to_excel(export_file, index = True, header=True)
 
 
-        def baz_info(event):
-            mb.showinfo("Описание","Сохраняет таблицу с описательной статистикой(среднее, стандартное отклонение, количество наблюдений, минимальное, максимальное и квартили) числовых столбцов.")
+        # def baz_info(event):
+        #     mb.showinfo("Описание","Сохраняет таблицу с описательной статистикой(среднее, стандартное отклонение, количество наблюдений, минимальное, максимальное и квартили) числовых столбцов.")
 
 
         def analis_wix(event):
@@ -132,8 +186,8 @@ class Kowalski_analis(tk.Toplevel):
             plt.xticks(a, n, rotation=75)
             plt.show()
 
-        def wix_info(event):
-            mb.showinfo("Описание","Используются в описательной статистике и позволяют быстро исследовать несколько наборов данных в графическом виде. Показывает верхние и нижние границы, квартили и медиану.")
+        # def wix_info(event):
+        #     mb.showinfo("Описание","Используются в описательной статистике и позволяют быстро исследовать несколько наборов данных в графическом виде. Показывает верхние и нижние границы, квартили и медиану.")
 
 
         def analis_gis(event):
@@ -161,13 +215,13 @@ class Kowalski_analis(tk.Toplevel):
             plt.show()
 
 
-        def gis_info(event):
-            mb.showinfo("Описание","Упорядоченная гистограмма эффективно передает порядок ранжирования элементов. Выводит столбцы по возрастанию.")
+        # def gis_info(event):
+        #     mb.showinfo("Описание","Упорядоченная гистограмма эффективно передает порядок ранжирования элементов. Выводит столбцы по возрастанию.")
 
         label_analis = ttk.Label(self, text='Выберите анализ: ')
         label_analis.grid(row=0, column=0)
-        label_info = ttk.Label(self, text='Чтобы посмотреть описание, нажмите на кнопку правой кнопкой мыши')
-        label_info.grid(row=7, column=0, rowspan=3, columnspan=3)
+        # label_info = ttk.Label(self, text='Чтобы посмотреть описание, нажмите на кнопку правой кнопкой мыши')
+        # label_info.grid(row=7, column=0, rowspan=3, columnspan=3)
         first_stolb = ttk.Combobox(self, values=['Product Code','Manufacturer','Country','Model','OS', 'Storage', 'Diagonal', 'CPU', 'RAM', 'Amount'], width=17)
         first_stolb.grid(row=1, column=1)
         second_stolb = ttk.Combobox(self, values=['Storage', 'Diagonal', 'RAM', 'Amount'], width=17)
@@ -190,26 +244,31 @@ class Kowalski_analis(tk.Toplevel):
         base_stolb = ttk.Button(self, text='Столбчатая Диаграмма')
         base_stolb.grid(row=1, column=0)
         base_stolb.bind('<Button-1>', analis_stolb)
-        base_stolb.bind('<Button-3>', stolb_info)
+        # base_stolb.bind('<Button-3>', stolb_info)
+        base_stolb_ttp = CreateToolTip(base_stolb, 'Показывает график, представленный прямоугольными зонами, высоты которых пропорциональны величинам, которые они отображают.')
         base_svod = ttk.Button(self, text='Сводная таблица')
         base_svod.grid(row=3, column=0)
         base_svod.bind('<Button-1>', analis_svod)
-        base_svod.bind('<Button-3>', svod_info)
+        # base_svod.bind('<Button-3>', svod_info)
+        base_svod_ttp = CreateToolTip(base_svod, 'Резюмирует данные, представленные в исходной таблице, показывает среднее значение по конечному столбцу.')
         base_ras = ttk.Button(self, text='Диаграмма рассеяния')
         base_ras.grid(row=2, column=0)
         base_ras.bind('<Button-1>', analis_rasseivanie)
-        base_ras.bind('<Button-3>', rasseivanie_info)
+        # base_ras.bind('<Button-3>', rasseivanie_info)
+        base_ras_ttp = CreateToolTip(base_ras, 'Показывает взаимосвязь между двумя или тремя переменными, цвет и размер точек отображают значение третьей переменную. Если введены 2 параметра, вместо третьего используется количество повторений.')
 
 #Поля базовой статистики
         baz_stat = ttk.Button(self, text='Базовая статистка', width = 90)
         baz_stat.bind('<Button-1>', analis_baz)
-        baz_stat.bind('<Button-3>', baz_info)
+        # baz_stat.bind('<Button-3>', baz_info)
         baz_stat.grid(row=4, column=0, columnspan=4)
+        baz_stat_ttp = CreateToolTip(baz_stat, 'Сохраняет таблицу с описательной статистикой(среднее, стандартное отклонение, количество наблюдений, минимальное, максимальное и квартили) числовых столбцов.')
 #Поля диаграммы Бокса-Вискера
         wix_stat = ttk.Button(self, text='Бокса-Вискера')
         wix_stat.grid(row=5, column=0)
+        wix_stat_ttp = CreateToolTip(wix_stat, 'Используются в описательной статистике и позволяют быстро исследовать несколько наборов данных в графическом виде. Показывает верхние и нижние границы, квартили и медиану.')
         wix_stat.bind('<Button-1>', analis_wix)
-        wix_stat.bind('<Button-3>', wix_info)
+        # wix_stat.bind('<Button-3>', wix_info)
         stolb_1_wix = ttk.Combobox(self, values=['Product Code','Manufacturer','Country','Model','OS', 'Storage', 'Diagonal', 'CPU', 'RAM', 'Amount'], width=17)
         stolb_1_wix.grid(row=5, column=1)
         stolb_2_wix = ttk.Combobox(self, values=['Storage', 'Diagonal', 'RAM', 'Amount'], width=17)
@@ -218,7 +277,8 @@ class Kowalski_analis(tk.Toplevel):
         gis_stat = ttk.Button(self, text='Гистограмма')
         gis_stat.grid(row=6, column=0)
         gis_stat.bind('<Button-1>', analis_gis)
-        gis_stat.bind('<Button-3>', gis_info)
+        # gis_stat.bind('<Button-3>', gis_info)
+        gis_stat_ttp = CreateToolTip(gis_stat, 'Упорядоченная гистограмма эффективно передает порядок ранжирования элементов. Выводит столбцы по возрастанию.')
         stolb_1_gis = ttk.Combobox(self, values=['Manufacturer', 'Country', 'Model', 'OS', 'CPU'], width=17)
         stolb_1_gis.grid(row=6, column=1)
         stolb_2_gis = ttk.Combobox(self, values=['Storage', 'Diagonal', 'RAM', 'Amount'], width=17)
