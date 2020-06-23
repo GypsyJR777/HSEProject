cbae02ae675a4c449745316f21d3d700bb2134e9
Anthony <anthonybudin-18@yandex.ru>
2020-06-23 23:00:26 +0300

    докстринг

--- Library/__init__.py
+++ Library/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 23 14:19:24 2020
-
-@author: ivand
+Библиотека, содержащая модуль:
+bd - формирование базы данных
 """
 from Library import bd
 __all__ = ["bd"]
